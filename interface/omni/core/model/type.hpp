9324dd823c748e5141afd2b6dd71e48f62ca173e
Daniel Albuschat <d.albuschat@gmail.com>
2014-06-12 07:06:47 +0200

    Documented the class "type"

--- interface/omni/core/model/type.hpp
+++ interface/omni/core/model/type.hpp
@@ -15,13 +15,28 @@ namespace omni {
 namespace core {
 namespace model {
 
+    /**
+    @class type type.hpp omni/core/model/type.hpp
+    @brief A type defines the properties of types that can be used for variables, function-parameters, function-return-values, etc.
+
+    <b>Important:</b> Always use type::sharedBasicType or context::sharedBasicType to get a type-object for any builtin-types.
+
+
+    The type_class differentiates between a few basic built-in types and user-defined types (classes).
+    (Currently, classes are not yet implemented for omni. Once they will be implemented, a new class class_type will be derived from
+    type that implements various functionality and properties that classes offer.)
+
+    A type is always strictly coupled with a context and, in contrast to most entities, can not exist without a context.
+    Builtin-types are not defined in any module, hence getModule () always returns nullptr.
+    Likely, builtin-classes do not have parents, hence getParent () always returns nullptr, too.
+    **/
     class OMNI_CORE_API type : public entity, public boost::noncopyable {
     public:
         type (context & context, type_class typeClass, unsigned int indirectionLevel = 0);
         virtual ~ type ();
 
-        virtual context * getContext ();
-        virtual const context * getContext () const;
+        context * getContext () override;
+        const context * getContext () const override;
 
         domain getDomain () const override;
 
@@ -30,13 +45,13 @@ namespace model {
 
         void setComponent (domain domain, std::string name, std::shared_ptr <entity> entity) override;
 
-        static std::shared_ptr <type> sharedBasicType (context & context, type_class typeClass, unsigned int indirectionLevel = 0);
-
-        llvm::Type * llvmType ();
-
         type_class getTypeClass () const;
 
         unsigned int getIndirectionLevel () const;
+
+        static std::shared_ptr <type> sharedBasicType(context & context, type_class typeClass, unsigned int indirectionLevel = 0);
+
+        llvm::Type * llvmType ();
 
     private:
         context & _context;
--- source/omni/core/model/type.cpp
+++ source/omni/core/model/type.cpp
@@ -8,10 +8,10 @@
 #include <sstream>
 
 namespace {
-/**
+/*
 Returns the type that points to baseType with the given indirectionLevel. For example, if baseType is int32 and indirectionLevel is 1,
 the result is int32*. If indirection is 2, the result is int32** and if indirection is 0, the result is int32.
-**/
+*/
 llvm::Type * addIndirection (llvm::Type * baseType, unsigned int indirectionLevel)
 {
     llvm::Type * result = baseType;
@@ -20,13 +20,20 @@ llvm::Type * addIndirection (llvm::Type * baseType, unsigned int indirectionLeve
     }
     return result;
 }
-}
+} // anonymous namespace
 
 /**
-Initializes this type with the given type_class and indirectionLevel.
+@brief Initializes this type with the given type_class and indirectionLevel.
+
+Neither the typeClass, nor the indirectionLevel can be changed later. If you need a type with a different typeClass or indirectionLevel,
+use another call to sharedBasicType for built-in types.
+
+<b>Important:</b> You should always use sharedBasicType instead of instantiating your own type object.
+
+@param context The context that this type is used in.
 @param typeClass The type class of the desired type. @see type_class
-@param indirectionLevel The level of pointer-indirection. This is only for internal usage and for representing types imported from external C code. 
-The Omni Language itself does not use types with an indiretionLevel > 0. Instead, "optionals" and "references" are used.
+@param indirectionLevel The level of pointer-indirection. This is only for internal usage and for representing types imported from external C code.
+The Omni Language itself does not use types with an indiretionLevel > 0. Instead, "optionals" and "references" are used. (Not yet implemented.)
 **/
 omni::core::model::type::type (omni::core::context & context, omni::core::model::type_class typeClass, unsigned int indirectionLevel) :
     entity (),
@@ -42,7 +49,6 @@ omni::core::model::type::type (omni::core::context & context, omni::core::model:
 omni::core::model::type::~ type ()
 {
 }
-
 
 omni::core::domain omni::core::model::type::getDomain () const
 {
@@ -75,8 +81,35 @@ void omni::core::model::type::setComponent (omni::core::domain domain, std::stri
 }
 
 /**
-Returns a shared_ptr for a instance of type for the desired context and type_class.
+@rief Returns the type_class of this type.
+@return The type_class of this type.
+@see type_class
+**/
+omni::core::model::type_class omni::core::model::type::getTypeClass () const
+{
+    return _typeClass;
+}
+
+/**
+@brief Returns the pointer indirection-level of this type.
+E.g. when the type_class is t_signedInt and the indirectionLevel is 2, the underlying C-type would be "int**".
+
+@return The pointer indirection-level of this type.
+**/
+unsigned int omni::core::model::type::getIndirectionLevel () const
+{
+    return _indirectionLevel;
+}
+/**
+@brief Returns a shared_ptr for a instance of type for the desired context and type_class.
+
 Short-hand for creating a type with new type(context, typeClass, indirectionLevel); with the additional benefit that types are cached in the context.
+You should always use sharedBasicType instead of instantiating your own type object.
+
+@param context The context that this type is used in.
+@param typeClass The type class of the desired type. @see type_class
+@param indirectionLevel The level of pointer-indirection. This is only for internal usage and for representing types imported from external C code.
+The Omni Language itself does not use types with an indiretionLevel > 0. Instead, "optionals" and "references" are used. (Not yet implemented.)
 @see context::sharedBasicType(type_class);
 **/
 std::shared_ptr <omni::core::model::type> omni::core::model::type::sharedBasicType (context & context, type_class typeClass, unsigned int indirectionLevel)
@@ -85,6 +118,10 @@ std::shared_ptr <omni::core::model::type> omni::core::model::type::sharedBasicTy
 }
 
 
+/*
+Returns an llvm::Type that corresponds to this Omni type. For built-in types, these directly map to i1, i8, i16, i32, and so on.
+A pointer-asterisk is added per indirectionLevel. (E.g. type_class t_signedInt with indirectionLevel 2 becomes an i32** on x86).
+*/
 llvm::Type * omni::core::model::type::llvmType ()
 {
     switch (_typeClass) {
@@ -116,21 +153,4 @@ llvm::Type * omni::core::model::type::llvmType ()
     default:
         throw not_implemented_error (__FILE__, __FUNCTION__, __LINE__);
     }
-}
-
-/**
-Returns the type_class of this type.
-**/
-omni::core::model::type_class omni::core::model::type::getTypeClass () const
-{
-    return _typeClass;
-}
-
-/**
-Returns the pointer indirection-level of this type.
-E.g. when the type_class is t_signedInt and the indirectionLevel is 2, the underlying C-type would be "int**".
-**/
-unsigned int omni::core::model::type::getIndirectionLevel () const
-{
-    return _indirectionLevel;
 }
